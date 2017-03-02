b1731ec9be280e53942dd219b13d1d00abaf6a67
yanqiong <ma_yanqiong@163.com>
2017-03-02 14:13:52 +0800

    init project

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    transparent: true
+    transparent: false
   })
 
   // and load the index.html of the app.
